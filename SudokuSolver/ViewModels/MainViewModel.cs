a8cf0ceb0484d1c77cc9889772c30f36e1c43e21
GangleAbout <108070429+GangleAbout@users.noreply.github.com>
2024-05-23 22:17:44 +0100

    Remove MainWidowViewModel and replace with MainViewModel

---
+++ SudokuSolver/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SudokuSolver.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    // Main Window Fields
+    [ObservableProperty] private string _windowTitle = "Sudoku Solver";
+    [ObservableProperty] private int _widowWidth = 800;
+    [ObservableProperty] private int _widowHeight = 700;
+}
